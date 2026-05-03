c4589f21fc2d90f284e4ece73c90a8380ca33583
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2026-05-03 14:52:31 +0200

    eq api

--- Sources/Capture/audio/effects/audio-equalizer.swift
+++ Sources/Capture/audio/effects/audio-equalizer.swift
@@ -21,20 +21,20 @@ public struct AudioEqualizerFactory: Sendable {
     ) -> AudioEqualizer.Parametric {
         AudioEqualizer.Parametric(
             bands: [
-                .peak(
+                .lowshelf(
                     frequency: 110,
                     gain: low,
-                    q: 0.7
+                    q: 0.707
                 ),
-                .peak(
+                .bell(
                     frequency: 3_600,
                     gain: mid,
                     q: 1
                 ),
-                .peak(
+                .highshelf(
                     frequency: 12_000,
                     gain: high,
-                    q: 0.7
+                    q: 0.707
                 ),
             ]
         )
@@ -44,7 +44,20 @@ public struct AudioEqualizerFactory: Sendable {
 public extension AudioEqualizer {
     struct Band: Sendable, Hashable {
         public enum Kind: Sendable, Hashable {
-            case peak
+            case bell
+            case shelf(Shelf)
+            case pass(Pass)
+
+            public enum Shelf: Sendable, Hashable {
+                case low
+                case high
+            }
+
+            public enum Pass: Sendable, Hashable {
+                case low
+                case high
+                case band
+            }
         }
 
         public let kind: Kind
@@ -55,8 +68,8 @@ public extension AudioEqualizer {
         public init(
             kind: Kind,
             frequency: Float,
-            gain: Float,
-            q: Float
+            gain: Float = 0,
+            q: Float = 1
         ) {
             self.kind = kind
             self.frequency = frequency
@@ -64,15 +77,105 @@ public extension AudioEqualizer {
             self.q = q
         }
 
-        public static func peak(
+        public static func bell(
             frequency: Float,
             gain: Float,
             q: Float = 1
         ) -> AudioEqualizer.Band {
             AudioEqualizer.Band(
-                kind: .peak,
+                kind: .bell,
                 frequency: frequency,
                 gain: gain,
+                q: q
+            )
+        }
+
+        public static func shelf(
+            _ shelf: Kind.Shelf,
+            frequency: Float,
+            gain: Float,
+            q: Float = 0.707
+        ) -> AudioEqualizer.Band {
+            AudioEqualizer.Band(
+                kind: .shelf(
+                    shelf
+                ),
+                frequency: frequency,
+                gain: gain,
+                q: q
+            )
+        }
+
+        public static func lowshelf(
+            frequency: Float,
+            gain: Float,
+            q: Float = 0.707
+        ) -> AudioEqualizer.Band {
+            shelf(
+                .low,
+                frequency: frequency,
+                gain: gain,
+                q: q
+            )
+        }
+
+        public static func highshelf(
+            frequency: Float,
+            gain: Float,
+            q: Float = 0.707
+        ) -> AudioEqualizer.Band {
+            shelf(
+                .high,
+                frequency: frequency,
+                gain: gain,
+                q: q
+            )
+        }
+
+        public static func pass(
+            _ pass: Kind.Pass,
+            frequency: Float,
+            q: Float = 0.707
+        ) -> AudioEqualizer.Band {
+            AudioEqualizer.Band(
+                kind: .pass(
+                    pass
+                ),
+                frequency: frequency,
+                gain: 0,
+                q: q
+            )
+        }
+
+        public static func lowpass(
+            frequency: Float,
+            q: Float = 0.707
+        ) -> AudioEqualizer.Band {
+            pass(
+                .low,
+                frequency: frequency,
+                q: q
+            )
+        }
+
+        public static func highpass(
+            frequency: Float,
+            q: Float = 0.707
+        ) -> AudioEqualizer.Band {
+            pass(
+                .high,
+                frequency: frequency,
+                q: q
+            )
+        }
+
+        public static func bandpass(
+            frequency: Float,
+            q: Float = 1
+        ) -> AudioEqualizer.Band {
+            pass(
+                .band,
+                frequency: frequency,
                 q: q
             )
         }
@@ -178,38 +281,66 @@ private struct Biquad: Sendable {
         sampleRate: Int
     ) -> Biquad {
         switch band.kind {
-        case .peak:
-            return peak(
+        case .bell:
+            return bell(
                 frequency: band.frequency,
                 gain: band.gain,
+                q: band.q,
+                sampleRate: sampleRate
+            )
+
+        case .shelf(.low):
+            return lowshelf(
+                frequency: band.frequency,
+                gain: band.gain,
+                q: band.q,
+                sampleRate: sampleRate
+            )
+
+        case .shelf(.high):
+            return highshelf(
+                frequency: band.frequency,
+                gain: band.gain,
+                q: band.q,
+                sampleRate: sampleRate
+            )
+
+        case .pass(.low):
+            return lowpass(
+                frequency: band.frequency,
+                q: band.q,
+                sampleRate: sampleRate
+            )
+
+        case .pass(.high):
+            return highpass(
+                frequency: band.frequency,
+                q: band.q,
+                sampleRate: sampleRate
+            )
+
+        case .pass(.band):
+            return bandpass(
+                frequency: band.frequency,
                 q: band.q,
                 sampleRate: sampleRate
             )
         }
     }
 
-    static func peak(
+    static func bell(
         frequency: Float,
         gain: Float,
         q: Float,
         sampleRate: Int
     ) -> Biquad {
-        guard sampleRate > 0,
-              frequency > 0,
-              q > 0 else {
+        guard let values = Values(
+            frequency: frequency,
+            q: q,
+            sampleRate: sampleRate
+        ) else {
             return bypass()
         }
-
-        let nyquist = Float(
-            sampleRate
-        ) / 2
-        let safeFrequency = max(
-            10,
-            min(
-                frequency,
-                nyquist - 10
-            )
-        )
 
         let amplitude = Float(
             pow(
@@ -219,25 +350,248 @@ private struct Biquad: Sendable {
                 )
             )
         )
-        let omega = 2 * Float.pi * safeFrequency / Float(
-            sampleRate
-        )
-        let sine = sinf(
-            omega
-        )
-        let cosine = cosf(
-            omega
-        )
-        let alpha = sine / (2 * q)
 
-        let b0 = 1 + alpha * amplitude
-        let b1 = -2 * cosine
-        let b2 = 1 - alpha * amplitude
-        let a0 = 1 + alpha / amplitude
-        let a1 = -2 * cosine
-        let a2 = 1 - alpha / amplitude
+        let b0 = 1 + values.alpha * amplitude
+        let b1 = -2 * values.cosine
+        let b2 = 1 - values.alpha * amplitude
+        let a0 = 1 + values.alpha / amplitude
+        let a1 = -2 * values.cosine
+        let a2 = 1 - values.alpha / amplitude
 
-        guard a0 != 0 else {
+        return normalized(
+            b0: b0,
+            b1: b1,
+            b2: b2,
+            a0: a0,
+            a1: a1,
+            a2: a2
+        )
+    }
+
+    static func lowshelf(
+        frequency: Float,
+        gain: Float,
+        q: Float,
+        sampleRate: Int
+    ) -> Biquad {
+        guard let values = Values(
+            frequency: frequency,
+            q: q,
+            sampleRate: sampleRate
+        ) else {
+            return bypass()
+        }
+
+        let amplitude = Float(
+            pow(
+                10.0,
+                Double(
+                    gain / 40
+                )
+            )
+        )
+        let root = sqrtf(
+            amplitude
+        )
+        let twoRootAlpha = 2 * root * values.alpha
+
+        let b0 = amplitude * (
+            (amplitude + 1)
+                - (amplitude - 1) * values.cosine
+                + twoRootAlpha
+        )
+        let b1 = 2 * amplitude * (
+            (amplitude - 1)
+                - (amplitude + 1) * values.cosine
+        )
+        let b2 = amplitude * (
+            (amplitude + 1)
+                - (amplitude - 1) * values.cosine
+                - twoRootAlpha
+        )
+        let a0 = (amplitude + 1)
+            + (amplitude - 1) * values.cosine
+            + twoRootAlpha
+        let a1 = -2 * (
+            (amplitude - 1)
+                + (amplitude + 1) * values.cosine
+        )
+        let a2 = (amplitude + 1)
+            + (amplitude - 1) * values.cosine
+            - twoRootAlpha
+
+        return normalized(
+            b0: b0,
+            b1: b1,
+            b2: b2,
+            a0: a0,
+            a1: a1,
+            a2: a2
+        )
+    }
+
+    static func highshelf(
+        frequency: Float,
+        gain: Float,
+        q: Float,
+        sampleRate: Int
+    ) -> Biquad {
+        guard let values = Values(
+            frequency: frequency,
+            q: q,
+            sampleRate: sampleRate
+        ) else {
+            return bypass()
+        }
+
+        let amplitude = Float(
+            pow(
+                10.0,
+                Double(
+                    gain / 40
+                )
+            )
+        )
+        let root = sqrtf(
+            amplitude
+        )
+        let twoRootAlpha = 2 * root * values.alpha
+
+        let b0 = amplitude * (
+            (amplitude + 1)
+                + (amplitude - 1) * values.cosine
+                + twoRootAlpha
+        )
+        let b1 = -2 * amplitude * (
+            (amplitude - 1)
+                + (amplitude + 1) * values.cosine
+        )
+        let b2 = amplitude * (
+            (amplitude + 1)
+                + (amplitude - 1) * values.cosine
+                - twoRootAlpha
+        )
+        let a0 = (amplitude + 1)
+            - (amplitude - 1) * values.cosine
+            + twoRootAlpha
+        let a1 = 2 * (
+            (amplitude - 1)
+                - (amplitude + 1) * values.cosine
+        )
+        let a2 = (amplitude + 1)
+            - (amplitude - 1) * values.cosine
+            - twoRootAlpha
+
+        return normalized(
+            b0: b0,
+            b1: b1,
+            b2: b2,
+            a0: a0,
+            a1: a1,
+            a2: a2
+        )
+    }
+
+    static func lowpass(
+        frequency: Float,
+        q: Float,
+        sampleRate: Int
+    ) -> Biquad {
+        guard let values = Values(
+            frequency: frequency,
+            q: q,
+            sampleRate: sampleRate
+        ) else {
+            return bypass()
+        }
+
+        let b0 = (1 - values.cosine) / 2
+        let b1 = 1 - values.cosine
+        let b2 = (1 - values.cosine) / 2
+        let a0 = 1 + values.alpha
+        let a1 = -2 * values.cosine
+        let a2 = 1 - values.alpha
+
+        return normalized(
+            b0: b0,
+            b1: b1,
+            b2: b2,
+            a0: a0,
+            a1: a1,
+            a2: a2
+        )
+    }
+
+    static func highpass(
+        frequency: Float,
+        q: Float,
+        sampleRate: Int
+    ) -> Biquad {
+        guard let values = Values(
+            frequency: frequency,
+            q: q,
+            sampleRate: sampleRate
+        ) else {
+            return bypass()
+        }
+
+        let b0 = (1 + values.cosine) / 2
+        let b1 = -(1 + values.cosine)
+        let b2 = (1 + values.cosine) / 2
+        let a0 = 1 + values.alpha
+        let a1 = -2 * values.cosine
+        let a2 = 1 - values.alpha
+
+        return normalized(
+            b0: b0,
+            b1: b1,
+            b2: b2,
+            a0: a0,
+            a1: a1,
+            a2: a2
+        )
+    }
+
+    static func bandpass(
+        frequency: Float,
+        q: Float,
+        sampleRate: Int
+    ) -> Biquad {
+        guard let values = Values(
+            frequency: frequency,
+            q: q,
+            sampleRate: sampleRate
+        ) else {
+            return bypass()
+        }
+
+        let b0 = values.alpha
+        let b1: Float = 0
+        let b2 = -values.alpha
+        let a0 = 1 + values.alpha
+        let a1 = -2 * values.cosine
+        let a2 = 1 - values.alpha
+
+        return normalized(
+            b0: b0,
+            b1: b1,
+            b2: b2,
+            a0: a0,
+            a1: a1,
+            a2: a2
+        )
+    }
+
+    static func normalized(
+        b0: Float,
+        b1: Float,
+        b2: Float,
+        a0: Float,
+        a1: Float,
+        a2: Float
+    ) -> Biquad {
+        guard a0 != 0,
+              a0.isFinite else {
             return bypass()
         }
 
@@ -258,5 +612,62 @@ private struct Biquad: Sendable {
             a1: 0,
             a2: 0
         )
+    }
+}
+
+private struct Values {
+    let frequency: Float
+    let q: Float
+    let sampleRate: Int
+    let omega: Float
+    let sine: Float
+    let cosine: Float
+    let alpha: Float
+
+    init?(
+        frequency: Float,
+        q: Float,
+        sampleRate: Int
+    ) {
+        guard sampleRate > 0,
+              frequency > 0,
+              q > 0 else {
+            return nil
+        }
+
+        let nyquist = Float(
+            sampleRate
+        ) / 2
+        let safeFrequency = max(
+            10,
+            min(
+                frequency,
+                nyquist - 10
+            )
+        )
+
+        guard safeFrequency.isFinite,
+              safeFrequency > 0,
+              safeFrequency < nyquist else {
+            return nil
+        }
+
+        let omega = 2 * Float.pi * safeFrequency / Float(
+            sampleRate
+        )
+        let sine = sinf(
+            omega
+        )
+        let cosine = cosf(
+            omega
+        )
+
+        self.frequency = safeFrequency
+        self.q = q
+        self.sampleRate = sampleRate
+        self.omega = omega
+        self.sine = sine
+        self.cosine = cosine
+        self.alpha = sine / (2 * q)
     }
 }
